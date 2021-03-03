84b9d317c7b3f543b894ea0a4f73fb580770f119
James Ponter <50483575+jponter@users.noreply.github.com>
2021-03-03 15:44:09 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TimecardCheck
+{
+    class Entry
+    {
+        public string Email = "";
+        public int Count = 0;
+
+        public Entry(string email)
+        {
+            Email = email;
+            Count = 1;
+        }
+    }
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            string line;
+            bool late = false;
+
+            List<Entry> entries = new List<Entry>();
+
+
+
+
+            Console.WriteLine("Reading File");
+
+            //open and read the file
+            System.IO.StreamReader file = new System.IO.StreamReader(@".\TimeCard.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Contains("TimeCard AlertAPP  09:15"))
+                {
+                    //we are in a late session
+                    late = true;
+                }
+                else if (line.Contains("TimeCard AlertAPP  08:15"))
+                {
+                    //we are in a late session
+                    late = true;
+                }
+                else if (line.Contains("red_circle") && late == true)
+                {
+                    bool found = false;
+                    String splitline = line.Split(' ')[1];
+                    if (entries.Count == 0) { entries.Add(new Entry(splitline)); }
+
+                    for (int i = 0; i < entries.Count; i++)
+                    {
+                        if (entries[i].Email == splitline)
+                        {
+                            entries[i].Count++;
+                            found = true;
+                        }
+                    }
+
+                    if (found == false)
+                    {
+                        entries.Add(new Entry(splitline));
+                    }
+
+
+                    //System.Console.WriteLine(splitline);
+
+                }
+                else if (line.Contains("TimeCard AlertAPP  18:15"))
+                {
+                    late = false;
+                }
+                else if (line.Contains("TimeCard AlertAPP  17:15"))
+                {
+                    late = false;
+                }
+                else
+                {
+                    //discard line
+                }
+
+
+
+
+            }
+
+
+
+
+
+            //Console.ReadKey();
+            for (int i = 0; i < entries.Count; i++)
+            {
+                Console.WriteLine(entries[i].Email + " " + entries[i].Count);
+            }
+            Console.ReadKey();
+        }
+    }
+}
